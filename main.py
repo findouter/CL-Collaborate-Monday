8a18be53ba5f6cb80266a5efe2a3fd929ef76faa
findouter <64108589+findouter@users.noreply.github.com>
2024-01-09 01:59:32 +0000

    Added the GoFish class in mksGame file. Added playability in main().

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 #this is the default page!
-from mk-s-game import *
+from mksGame import GoFish
 
 
 def main():
-  #game = Game()
-  pass
+  #menu function
+  #let user choose a game to play
+  choice = input("Please select a game to play: \n"
+       + "GoFish")
+  if choice == "GoFish":
+    game = GoFish()
+  
+
+  
+  #run the selected game
+  game.run()
+  
+
 
 
 if __name__ == "__main__":
